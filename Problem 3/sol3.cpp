896930aa887b5e4e2ec3d569efc55bc689cf256b
[Lalit-garg] <[lalitmohan4398@gmail.com]>
2020-01-18 17:44:11 +0530

    Problem 3

---
+++ Problem 3/sol3.cpp
@@ -0,0 +1,13 @@
+    int searchInsert(vector<int>& nums, int target) {
+                int i;
+        if(nums.size()==0)
+            return 0;
+        for(i=0;i<nums.size();i++)
+        {
+            if(nums[i]==target)
+                return i;
+            else if(nums[i]>target)
+                return i;
+        }
+        return i;
+    }
